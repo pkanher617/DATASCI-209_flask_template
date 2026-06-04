78c787ce2d2b3e7a30ea360b943af3cec9e8fc14
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2026-06-03 17:25:07 -0700

    adding map

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import os
+import pandas as pd
 app = Flask(__name__)
+_HERE = os.path.dirname(os.path.realpath(__file__))
 
 @app.route('/')
 def w209():
     file='about9.jpg'
     return render_template('w209.html',file=file)
+
+@app.route("/map")
+def map_page():
+    return render_template("map.html")
+
+@app.route("/getData/<int:year>")
+def getData(year):
+    revenue = pd.read_csv(os.path.join(_HERE, "..", "static", "data", "1_Revenues.csv"))
+    if year < 1942 or year > 2008:
+        return "Error in the year range"
+    filtered = revenue[revenue["Year4"] == year][
+        ["Name", "Year4", "Total Revenue", "Population (000)"]
+    ]
+    return filtered.to_json(orient="records")
 
 if __name__ == '__main__':
     app.run()
